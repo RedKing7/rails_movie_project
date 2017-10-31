a46a9008bc6ace7396b38205744b32337d8b8e24
Rory Johnson <redkjohnson@gmail.com>
2017-10-30 22:18:26 -0400

    added index page for movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+##User.destroy_all
+##FavoriteMovie.destroy_all
+
+movies = Movie.create([
+  {
+    title: 'Star Wars',
+    genre: 'Sci-Fi',
+    year: 1977
+  },
+  {
+    title: 'Fellowship of the Rings',
+    genre: 'fantasy',
+    year: 2001
+  }
+])
